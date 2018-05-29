93db83b4fb46f61c82275a4914003260fb7ef78e
Dianne Dietrich <dd388@cornell.edu>
2018-05-29 11:50:22 -0400

    So many reorgs, but maybe I'm on track now

---
+++ validator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Import modules we need
+
+# Rule: Does every file have an extension?
+
+# Rule: Every file is at least zero bytes
+
+
+# Rule: There are no hidden files here
+
+
+# Rule: No filename has any sort of whitespace
+
+
+
+# Main function
