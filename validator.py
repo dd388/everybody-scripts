5008aa9a7dd637b2bdd1f2e774abe30dcbf74401
farrell <matthew.j.farrell@duke.edu>
2018-06-21 09:51:32 -0400

    added basic arguments for input and output

--- validator.py
+++ validator.py
@@ -2,6 +2,7 @@
 
 # Import modules we need
     # What should we import to get the job(s) done?
+import argparse # Definitely need a module like argparse if we want to supply arguments--f
 
 # Rule: Does every file have an extension?
 # Rule: Every file is at least zero bytes
@@ -26,6 +27,16 @@ def validator():
     # Other things to think about
         # How should we handle unexpected input?
         # How do we write/run tests on this?
+
+parser.add_argument("-d","--directory", help="Path to input directory.")
+parser.add_argument("-o","--output", help="Path to and filename for output file. If unused, output will be to stdout")
+args = parser.parse_args()
+
+if args.directory: #assigning the path to input directory to a variable. I'm not declaring it global, but I believe because the variable is being declared and assigned outside of a function, it's implicitly global. Not sure if that's desirable, not sure what other approach there could be if the source directory is supposed to be used by multiple functions.--f
+	inputDir = args.directory
+
+if args.output:
+	outputFile = args.output
 
 if __name__ == '__main__':
     validator()
