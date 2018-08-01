9cb63dccf6b6cf1303830687a203adf213df8e60
Shira Peltzman <shira.peltzman@gmail.com>
2018-08-01 11:22:08 -0700

    Update whitespace-test.py

--- whitespace-test.py
+++ whitespace-test.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+# attempt at removing whitespace from all filenames within a given directory
+
+# ask user to input filename
+  # question for group -- this may not be the right place to ask for user input since presumably this would happen at the beginning of the script
+  # also, we decided to do this for a single file to keep it simple. we can and should change this to a directory in the final script
+sourcefile = input('Enter the file name:')
+
+
+# determine whether filename includes whitespace 
+
+# if whitespace is present, remove whitespace -- although, question for group: would we simply want the whitespace removed, or replaced with a dash or underscore?
+
+# if no whitespace is present, continue 
+
+
 
