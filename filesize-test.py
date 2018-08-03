b40852f13d48d0d5fce5e65bac69398652da088f
mjhugh <32968533+mjhugh@users.noreply.github.com>
2018-08-03 11:20:35 -0700

    Update filesize-test.py

--- filesize-test.py
+++ filesize-test.py
@@ -8,16 +8,11 @@ import os
 filename = f in files
 def validateFileSize(filename):
   filesize = os.path.getsize("/filepath")
-  if filesize > 0
-    print("All files bigger than 0 bytes")
+  if filesize == 0
+    print("All files with 0 bytes")
     print (filename, "is", filesize, "bytes")
 
 For filename is os.listdir(directory):
     for filename
         validateFileSize
 
-
-## other bits of code and notes
-        def getSize(filename):
-            st = os.stat(filename)
-            return st.st_size
