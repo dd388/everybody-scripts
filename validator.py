149dd92e1d9f8fc4b4b945291489fc80bf3af647
Farrell <matthew.j.farrell@duke.edu>
2018-10-23 17:24:28 -0400

    Add pathlib call to main function.

--- validator.py
+++ validator.py
@@ -3,14 +3,16 @@
 # Import modules we need
     # What should we import to get the job(s) done?
 import argparse # Definitely need a module like argparse if we want to supply arguments--f
+from pathlib import Path
 
+# Rule: if
 # Rule: Does every file have an extension?
 # Rule: Every file is at least zero bytes
 # Rule: There are no hidden files here
 def findHidden(f): #I'm assuming here that validator() is looping through files
     if f.startswith("."):
         print("!!!!! HIDDEN FILE FOUND !!!!!, ", f)
-    
+
 # Rule: No filename has any sort of whitespace
     # What input does this need?
     # What should the output be?
@@ -31,7 +33,11 @@ def validator():
     if args.output:
         outputFile = args.output
 
-    # Some things we might want to do here:
+    p = Path(inputDir) # declares inputDir as a Path object
+    for i in p.glob(**/*.*): # i in this statement should now be every file in p.
+        print(i.name) # comment this line and make this loop do something else, like call one of our other functions
+
+        # Some things we might want to do here:
         # Gather and check user input
         # Set up output filename (see if we're overwriting anything?)
         # Get a list of files we're working with
