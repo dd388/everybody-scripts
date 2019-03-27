d365308b17c29b4ce83c9f985152ff0df114ddfd
Dianne Dietrich <dd388@cornell.edu>
2019-03-27 10:58:01 -0400

    update validator code with user input features; cleaned up code

--- validator.py
+++ validator.py
@@ -1,43 +1,67 @@
 #!/usr/bin/env python3
 
-# Import modules we need
-import argparse # Definitely need a module like argparse if we want to supply arguments--f
+import argparse
+import sys
+import csv
 from pathlib import Path
 
-# Rule: Does every file have an extension?
-# Rule: Every file is at least zero bytes
-# Rule: There are no hidden files here
-def findHidden(f): #I'm assuming here that validator() is looping through files
-    if f.startswith("."):
-        print("!!!!! HIDDEN FILE FOUND !!!!!, ", f)
+# Testing functions go here
+# Input: path to file
+# Output: True or False
 
-# Rule: No filename has any sort of whitespace
-    # What input does this need?
-    # What should the output be?
 
-# Output report
-    # What input should this get to produce a report?
-    # Does it produce the whole report or does it output line by line?
 
 # Main function
 def validator():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d","--directory", help="Path to input directory. If unused, input will be current directory.")
-    parser.add_argument("-o","--output", help="Path to and filename for output file. If unused, output will be to stdout")
+    parser.add_argument("-d","--directory",
+                        help="Path to input directory. If unused, " + 
+                             "input will be current directory.")
+    parser.add_argument("-o","--output",
+                        help="Path to and filename for output file. " + 
+                             "If unused, output will be to stdout")
     args = parser.parse_args()
 
     if args.directory:
-        inputDir = args.directory
+        inputDir = Path(args.directory)
+        if not inputDir.is_dir():
+            sys.exit("Exiting: Input directory not valid.")
     else:
-        pass # TODO Make current directory
+        print("No input directory specified. Using current directory: ")
+        print(Path.cwd())
+        print("") # spacing is friendly.
+        inputDir = Path.cwd()
     if args.output:
-        outputFile = args.output
+        outputTarget = Path(args.output)
+        if outputTarget.exists():
+            print("Input file already exists.")
+            print("Do you want to overwrite this file?")
+            overwrite = None
+            while overwrite not in ('y', 'n'):
+                overwrite = input("Please enter yes or no. ")
+                overwrite = overwrite[0].casefold()
+                if overwrite == 'y':
+                    outputFile = open(outputTarget, 'w')
+                elif overwrite == 'n':
+                    sys.exit("Exiting: Will not overwrite existing file.") 
+                else:
+                    pass # user didn't remotely answer the question.
+            print("") # spacing is still friendly.
+        else:
+            outputFile = open(outputTarget, 'w')
     else:
-        pass # TODO Make stdout
+        outputFile = sys.stdout
 
-    p = Path(inputDir) # declares inputDir as a Path object
-    for i in p.glob('**/*.*'): # i in this statement should now be every file in p.
-        print(i.name) # comment this line and make this loop do something else, like call one of our other functions
+    print("TESTS:")
+    print("inputDir : ", inputDir)
+    print("outputFile : ", outputFile)
+
+
+    # TODO: Identify files in inputDir
+    #       Create a loop for them
+    #       Run each test, get and store True/False values
+    #       Write a CSV (use the CSV module)
+    #       Good job.
 
 if __name__ == '__main__':
     validator()
